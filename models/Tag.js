61cd97017f2025523649c8b70da4fd62b074f8a7
soulslurpee <charles.d.thompson87@gmail.com>
2022-07-03 15:20:05 -0400

    finished rest of models

--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,41 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
-class Tag extends Model {}
+class ProductTag extends Model {}
 
-Tag.init(
+ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: 'product_tag',
   }
 );
 
-module.exports = Tag;
+module.exports = ProductTag;
